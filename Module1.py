92e47e674e17f5970c1a1d447f4da50713ea42e9
Lauren <lhoughla@uccs.edu>
2025-10-06 13:19:44 -0600

    updates

--- Module1.py
+++ Module1.py
@@ -1,14 +1,15 @@
 #Part 1 Addition and Subtraction
 print("Part1")
-print("Enter a number:", end='')
+print("Enter a number: ", end='')
 num1=float(input())
 
-print("Enter a second number:", end='')
+print("Enter a second number: ", end='')
 num2=float(input())
 
 print("Adding", num1, "to", num2, "is:", (num1 + num2))
 print("Subtracting", num1, "by", num2, "is:", (num1 - num2))
 
+print()
 #Part 2 Multiplication and Division
 print("Part 2")
 print("Enter a number:", end='')
@@ -17,5 +18,5 @@ num1=float(input())
 print("Enter a second number:", end='')
 num2=float(input())
 
-print("Multiplying", num1, "by", num2, ":", (num1 * num2))
+print("Multiplying", num1, "by", num2, "is:", (num1 * num2))
 print("Dividing", num1, "by", num2, "is:", (num1 / num2))
