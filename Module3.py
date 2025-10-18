1709b0b515bcc7bd7f5761d3fa51eda52d614f6e
Lauren <lhoughla@uccs.edu>
2025-10-18 11:39:59 -0600

    updated to check for invalid num

--- Module3.py
+++ Module3.py
@@ -2,6 +2,11 @@
 print("Part 1: Total for Meal")
 print("Enter the charge for your food:")
 food_charge = float(input())
+
+#Check if number is <=0
+while food_charge<=0:
+    print("Your food charge cannot be less than or equal to 0\nPlease enter the amount:")
+    food_charge = float(input())
 tip = food_charge * 0.18
 print("An 18% tip for ${:.2f}".format(food_charge),"would be: ${:.2f}".format(tip))
 tax = food_charge * 0.07
@@ -14,9 +19,21 @@ print("\nPart 2: 24-hour Alarm Clock")
 print("What is the current time in hours (0-23)?")
 current_time = int(input())
 
+#Check if number is outside range
+while current_time<0 or current_time>23:
+    print("Please enter a valid time:")
+    current_time = int(input())
+
 print("In how many hours do you want the alarm to go off?")
 alarm_hours = int(input())
 
+#Check if number is <= 0
+while alarm_hours<=0:
+    print("Please enter a valid number:")
+    alarm_hours = int(input())
+
+#set alarm time
 alarm_time = (current_time + alarm_hours) % 24
+
 #Print formatted time alarm will go off
 print("Your alarm will go off at: {:02d}:00".format(alarm_time))
