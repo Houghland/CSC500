61810e2458b332195feea15ab78b4b981da49c09
Lauren <lhoughla@uccs.edu>
2025-10-18 12:50:26 -0600

    methods added

--- Module4.py
+++ Module4.py
@@ -4,29 +4,41 @@ class item:
     item_price = 0
     item_quantity = 0
 
-#Check that input is valid
-def valid_input(prompt, value_type, condition):
-    value = value_type(input(prompt))
-    while not condition(value):
-        value = value_type(input("Please enter a valid value:"))
-    return value
+    def print_item_cost(item):
+        total_cost = item.item_price * item.item_quantity
+        print(f"{item.item_name} {item.item_quantity:.0f} @ ${item.item_price:.2f} = ${total_cost:.2f}")
+    
+    def valid_input(prompt, value_type, condition):
+        value = value_type(input(prompt))
+        while not condition(value):
+            value = value_type(input("Please enter a valid value:"))
+        return value
 
-#objects
+
+def print_total_cost(item1, item2):
+        total = ((item1.item_price * item1.item_quantity) + (item2.item_price * item2.item_quantity))
+        print(f"\nTotal cost: ${total:.2f}")
+
+#define objects
 item1 = item()
 item2 = item()
 
+
+#get inputs
 print("Item 1")
-item1.item_name = valid_input("Enter the item name: ", str, lambda x: x.strip() != "")
-item1.item_price = valid_input("Enter the item price: ", float, lambda x: x > 0)
-item1.item_quantity = valid_input("Enter the item quantity: ", int, lambda x: x > 0)
+item1.item_name = item.valid_input("Enter the item name: ", str, lambda x: x.strip() != "")
+item1.item_price = item.valid_input("Enter the item price: ", float, lambda x: x > 0)
+item1.item_quantity = item.valid_input("Enter the item quantity: ", int, lambda x: x > 0)
 
 
 print("Item 2")
-item2.item_name = valid_input("Enter the item name: ", str, lambda x: x.strip() != "")
-item2.item_price = valid_input("Enter the item price: ", float, lambda x: x > 0)
-item2.item_quantity = valid_input("Enter the item quantity: ", int, lambda x: x > 0)
+item2.item_name = item.valid_input("Enter the item name: ", str, lambda x: x.strip() != "")
+item2.item_price = item.valid_input("Enter the item price: ", float, lambda x: x > 0)
+item2.item_quantity = item.valid_input("Enter the item quantity: ", int, lambda x: x > 0)
+
 
 #calculations
 print("Total Cost:\n")
-print(item1.item_name, item1.item_price, "@ ${:.2f}".format(item1.item_quantity), "=", (item1.item_price * item1.item_quantity))
-print(item2.item_name, item2.item_price, "@ ${:.2f}".format(item2.item_quantity), "=", (item2.item_price * item2.item_quantity))
+item1.print_item_cost()
+item2.print_item_cost()
+print_total_cost(item1, item2)
